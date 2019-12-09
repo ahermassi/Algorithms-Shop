3e71bbe6ff7c7726f994294b7a56e6754b984307
Anouer Hermassi <ahermass@stevens.edu>
2019-12-09 11:20:39 -0500

    Merge sort - update

--- Sort/MergeSort.py
+++ Sort/MergeSort.py
@@ -1,34 +1,31 @@
-def merge_sort(arr):
-    if len(arr) == 1:
-        return
-    mid = len(arr) // 2
-    left = arr[:mid]
-    right = arr[mid:]
-    merge_sort(left)
-    merge_sort(right)
-    i = j = k = 0
-    # After the mergeSort functions above finish executing, merge the sorted left array and right array together
-    while i < len(left) and j < len(right):
-        if left[i] < right[j]:
-            arr[k] = left[i]
+def merge_sort(nums):
+    if len(nums) <= 1:
+        return nums
+    mid = len(nums) // 2
+    left_half = merge_sort(nums[:mid])
+    right_half = merge_sort(nums[mid:])
+    return merge(left_half, right_half)
+
+
+def merge(nums1, nums2):
+    n, m = len(nums1), len(nums2)
+    res = []
+    i = j = 0
+    while i < n and j < m:
+        if nums1[i] <= nums2[j]:
+            res.append(nums1[i])
             i += 1
         else:
-            arr[k] = right[j]
+            res.append(nums2[j])
             j += 1
-        k += 1
-    # Checking if any element was left
-    while i < len(left):
-        arr[k] = left[i]
-        i += 1
-        k += 1
-    while j < len(right):
-        arr[k] = right[j]
-        j += 1
-        k += 1
+    if i < n:
+        res.extend(nums1[i:])
+    if j < m:
+        res.extend(nums2[j:])
+    return res
 
 
 if __name__ == '__main__':
     arr = [64, 25, 12, 22, 11]
     print('Array before sorting: ', *arr)
-    merge_sort(arr)
-    print('Array after Merge Sort: ', *arr)
+    print('Array after Merge Sort: ', *merge_sort(arr))
