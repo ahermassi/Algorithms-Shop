3714506a5cfc16582c4ee7ad71893c8dc9236682
Anouer Hermassi <ahermass@stevens.edu>
2019-12-09 22:28:29 -0500

    Selection sort - update

--- Sort/SelectionSort.py
+++ Sort/SelectionSort.py
@@ -1,32 +1,34 @@
-def selection_sort(arr):
-    for i in range(len(arr)):
+def selection_sort(nums):
+    n = len(nums)
+    for i in range(n):
         min = i
         # Find the index of min element
-        for j in range(i + 1, len(arr)):
-            if arr[j] < arr[min]:
+        for j in range(i + 1, n):
+            if nums[j] < nums[min]:
                 min = j
-        arr[i], arr[min] = arr[min], arr[i]
+        nums[i], nums[min] = nums[min], nums[i]
 
 
-def selection_sort_recursive(arr, start):
-    if start == len(arr) - 1:
+def selection_sort_recursive(nums, start):
+    n = len(nums)
+    if start == n - 1:
         return
     min = start
-    for i in range(start + 1, len(arr)):
-        if arr[i] < arr[min]:
+    for i in range(start + 1, n):
+        if nums[i] < nums[min]:
             min = i
-    arr[start], arr[min] = arr[min], arr[start]
-    selection_sort_recursive(arr, start + 1)
+    nums[start], nums[min] = nums[min], nums[start]
+    selection_sort_recursive(nums, start + 1)
 
 
 if __name__ == '__main__':
-    arr = [64, 25, 12, 22, 11]
-    print('Array before sorting: ', *arr)
-    selection_sort(arr)
-    print('Array after Selection Sort: ', *arr)
+    nums = [64, 25, 12, 22, 11]
+    print('Array before sorting: ', *nums)
+    selection_sort(nums)
+    print('Array after Selection Sort: ', *nums)
 
-    arr = [64, 25, 12, 22, 11]
-    print('Array before recursive sorting: ', *arr)
-    selection_sort_recursive(arr, 0)
-    print('Array after recursive Selection Sort: ', *arr)
+    nums = [64, 25, 12, 22, 11]
+    print('Array before recursive sorting: ', *nums)
+    selection_sort_recursive(nums, 0)
+    print('Array after recursive Selection Sort: ', *nums)
 
